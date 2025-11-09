765c3e3725a736e2266366365ce2e531d0fd1c68
sudhirahirkar <sudhir.ahirkar28@gmail.com>
2025-11-09 13:17:38 +0530

    added listner for add-payment

--- verify-service/src/main/java/com/toll/verify/kafka/ChargeResponseListener.java
+++ verify-service/src/main/java/com/toll/verify/kafka/ChargeResponseListener.java
@@ -1,6 +1,36 @@
 package com.toll.verify.kafka;
 
-import com.toll.common.model.ChargeStatus;
+import com.toll.common.model.TagChargeResponse;
+import com.toll.verify.service.VerifyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ChargeResponseListener {
+
+    private final VerifyService verifyService;
+
+    @Transactional
+    @KafkaListener(
+            topics = "${payment.topics.response}",
+            containerFactory = "responseKafkaListenerFactory"
+    )
+    public void handlePaymentResponse(TagChargeResponse response) {
+
+        log.info("⬇️ Received TagChargeResponse eventId={} status={}",
+                response.getEventId(), response.getStatus());
+
+        // Centralize all business logic
+        verifyService.applyPaymentResult(response);
+    }
+}
+
+/*import com.toll.common.model.ChargeStatus;
 import com.toll.common.model.TagChargeResponse;
 import com.toll.verify.entity.TollTransaction;
 import com.toll.verify.repository.TollTransactionRepository;
@@ -81,7 +111,7 @@ public class ChargeResponseListener {
 
         txRepo.save(tx);
     }
-}
+}*/
 
 /*
 @Slf4j
